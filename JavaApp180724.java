fe356d6ced80c24973d5ad73b5b6fda3fc4aa6fc
打家豪 <41461893+ypafeuer@users.noreply.github.com>
2018-07-24 10:33:03 +0800

    Update JavaApp180724.java

--- JavaApp180724.java
+++ JavaApp180724.java
@@ -8,14 +8,14 @@ public class JavaApp180724 {
     }
     static void one(){     
         two();
-        JavaApplication12.two();
+        JavaApp180724.two();
         //three(); // non-static
         //JavaApplication12.four(); // non-static
     }
     static void two(){/**/}
     void three(){
         one();
-        JavaApplication12.two();
+        JavaApp180724.two();
         four();
         //JavaApplication12.four(); //non-static
     }
